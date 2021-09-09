976caff81b40b1d91f7a316fc08bc504c4d955f4
汪洋 <wangyang2010344@foxmail.com>
2021-09-09 22:22:53 +0800

    更新

---
+++ src/index.ts
@@ -0,0 +1,72 @@
+import { EOParseResult } from "mve-core/util"
+import { dom } from "mve-dom"
+import { simpleAdd } from "./simpleAdd"
+import { simpleTodo } from "./simpleTodo"
+import { todo } from "./todo"
+import { tree } from "./tree"
+
+export type Act={
+	title:string
+	url:string
+	render():EOParseResult<Node>
+}
+
+export function listActs(){
+	return dom({
+		type:"ul",
+		children:acts.map(v=>dom({
+			type:"li",
+			children:[
+				dom({
+					type:"a",
+					attr:{
+						href:"javascript:void(0)"
+					},
+					text:v.title,
+					event:{
+						click(){
+							location.hash="#"+v.url
+						}
+					}
+				})
+			]
+		}))
+	})
+}
+
+export const acts:Act[]=[
+	{
+		title:"首页",
+		url:"index",
+		render(){
+			return dom.root(function(me){
+				return {
+					type:"div",
+					children:[
+						listActs()
+					]
+				}
+			})
+		}
+	},
+	{
+		title:"简单的加法",
+		url:"simpleAdd",
+		render:simpleAdd
+	},
+	{
+		title:"简单的Todo",
+		url:"simpleTodo",
+		render:simpleTodo
+	},
+  {
+    title:"todo",
+    url:"todo",
+    render:todo
+  },
+  {
+    title:"tree",
+    url:"tree",
+    render:tree
+  }
+]
