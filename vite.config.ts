6b19a61ea2154cd1b1705463cb5567c7638dcc8f
wangyang <wangyang2010344@qq.com>
2021-09-17 16:09:31 +0800

    修正发布

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 export default defineConfig({
 	server:{
-		port:4000, 
-		fs: {
-      // Allow serving files from one level up to the project root
-      allow: ['.','..','/','/node_modules/']
-    }
+		port:4000
 	},
+	base:'./'
 })
