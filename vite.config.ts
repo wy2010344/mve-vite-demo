0447b0dbf46c657d70456598b686f198f5c6c409
wangyang <wangyang2010344@qq.com>
2021-10-15 20:26:29 +0800

    更新代码

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
+import { defineConfig,PluginOption,transformWithEsbuild } from 'vite'
 export default defineConfig({
 	server:{
 		port:4000
 	},
-	base:'./'
+	base:'./',
+	plugins:[
+		viteMveMdxPlugin()
+	]
 })
+
+import mdx from '@mdx-js/mdx'
+import {mdxStringifyMve} from './src/vite-mdx-mve-tsx-plugin/mdxTransfier'
+function viteMveMdxPlugin():PluginOption{
+	const compiler=mdx.createCompiler()
+	return {
+		name:"vite-mve-mdx-tsx-plugin",
+		enforce:"pre",
+		async transform(code,id){
+			if(/\.mdx?$/.test(id)){
+				const node=compiler.parse(code)
+				const nCode= ` 
+import {Dom,createElement,Svg} from 'mve-dom/tsxSupport'
+export default ${mdxStringifyMve(code)}
+				`
+				console.log(nCode)
+				const out=await transformWithEsbuild(nCode,id+".tsx")
+				return out
+			}
+		}
+	}
+}
