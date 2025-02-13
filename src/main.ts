123d7aa28534680dd48f8ca92a9be7256ac84e20
wy2010344 <wangyang2010344@foxmail.com>
2025-02-13 20:02:39 +0800

    save demos

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,58 @@
-import { clsOf, dom } from "mve-dom/index"
-import { orRun } from "mve-core/util"
-import { rootRoute } from "./index"
-import { initPrism } from "./prismHelper"
-import {locationHashBind} from 'mve-dom/router'
-const topAreaCls=clsOf("topArea")
+import { createRoot } from 'mve-dom'
+import demo1 from './demo1'
+import magnifiedDock from './magnified-dock'
+import observer from './oberver'
+import konvaDemo from './konva-demo'
+import canvas from './canvas'
+import changePortal from './changePortal'
+import clipDemo from './clipDemo'
+import tree from './tree'
+import absoluteDemo from './absoluteDemo'
+import canvas2 from './canvasDemo'
+import typeContain from './typeContain'
+import three from './three'
+import scrollDemo1 from './scroller-demo/demo1'
+import scrollDemo1p from './scroller-demo/demo1p'
+import pagingx from './scroller-demo/pagingx'
+import snapDemo from './scroller-demo/snapDemo'
+import scrollerDemo1 from './demos/scrollerDemo1'
+import reorderDemo from './demos/reorderDemo'
+import forceDemo from './d3/force/demo1'
+import forceDemo2 from './d3/force/demo2'
+import forceDemo3 from './d3/force/demo3'
+import forceDemo4 from './d3/force/demo4'
+import exitAnimate from './exitAnimate'
+import demo2 from './demos/demo2'
+import calendar from './demos/calendar'
+const app = document.querySelector<HTMLDivElement>('#app')!
 
-const root=dom.root(function(me){
-	return {
-		type:"div",
-		children:[
-			initPrism(),
-			dom({
-				type:"style",
-				text:`
-				.${topAreaCls}{
-					position:fixed;
-					top:0;
-					left:0;
-					width:100%;
-					height:100%
-				}
-				`
-			}),
-			rootRoute.render(me)
-		]
-	}
+const destroy = createRoot(app, () => {
+  calendar()
+  // demo2()
+  // scrollerDemo1()
+  // reorderDemo()
+  // changePortal()
+  // canvas()
+  // canvas2()
+  // exitAnimate()
+  // scrollDemo1()
+  // scrollDemo1p()
+  // pagingx()
+  // snapDemo()
+  // konvaDemo()
+  // observer()
+  // demo1()
+  // typeContain()
+  // tree()
+  // absoluteDemo(app)
+  // three()
+  // clipDemo()
+  // magnifiedDock()
+  // forceDemo()
+  // forceDemo2()
+  // forceDemo3()
+  // forceDemo4()
 })
 
-locationHashBind(rootRoute)
 
-document.body.appendChild(root.element)
-
-const destroy=orRun(root.init)
-if(destroy){
-	window.addEventListener("unload",destroy)
-}
+window.addEventListener("unload", destroy)
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,12 +1,1 @@
 /// <reference types="vite/client" />
-
-declare namespace JSX {
-	type IntrinsicElements=import("mve-dom/tsxSupport").JSX.IntrinsicElements
-	type Element=import("mve-dom/tsxSupport").JSX.Element
-	type ElementChildrenAttribute=import('mve-dom/tsxSupport').JSX.ElementChildrenAttribute
-}
-
-declare module '*.mdx'{
-	let MDXComponent:import("./vite-mdx-mve-tsx-plugin/visitTsx").MdxAstType[]
-	export default MDXComponent
-}
