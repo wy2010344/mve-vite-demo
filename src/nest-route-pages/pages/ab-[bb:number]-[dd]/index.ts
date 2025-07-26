04f837c03f7c26356700b2af003d659c4297e779
wy2010344 <wangyang2010344@foxmail.com>
2025-07-26 17:19:28 +0800

    some fix

--- src/nest-route-pages/pages/ab-[bb:number]-[dd]/index.ts
+++ src/nest-route-pages/pages/ab-[bb:number]-[dd]/index.ts
@@ -2,10 +2,10 @@ import { fdom, renderText } from "mve-dom";
 import { LeafLoaderParam } from "mve-helper";
 import { GetValue, KVPair } from "wy-helper";
 
-export default function (arg: GetValue<LeafLoaderParam>) {
+export default function (arg: LeafLoaderParam) {
   fdom.div({
     children() {
-      renderText`ab-${() => JSON.stringify(arg().query)}-cc`
+      renderText`ab-${() => JSON.stringify(arg.getQuery())}-cc`
     }
   })
 }
